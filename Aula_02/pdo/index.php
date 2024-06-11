93bbfc48b820b90ad5c985a37ffb799aea376fb1
RenonHG <renongusson@gmail.com>
2024-06-11 10:52:50 -0300

    CONEXAO BANCO PHP

--- Aula_02/pdo/index.php
+++ Aula_02/pdo/index.php
@@ -1,8 +1,8 @@
 <?php include_once './pages/header.html';
-
+$_ENV = parse_ini_file('.env');
 // PDO → php document object
 
-$conexao = new PDO("mysql:host=localhost;dbname=db_policebox", "root", "");
+$conexao = new PDO("mysql:host={$_ENV['HOST']};dbname={$_ENV['DATABASE']}", $_ENV['USER'], $_ENV['PASSWORD']);
 $query = "SELECT * FROM tb_produto";
 
 $produtos = $conexao-> query($query)->fetchAll();
